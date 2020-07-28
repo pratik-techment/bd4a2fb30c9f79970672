e2e39b21d48161fbe9bc536bd2b42d25717db2eb
Pratik Raut <pratik.raut@techment.com>
2020-07-28 07:56:38 -0700

    added answers logic

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,13 @@
 class AnswersController < ApplicationController
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.create(text_value: answer_params[:text_value], user_id: current_user.id, question_id: @question.id)
+    redirect_to question_path(@question)
+   end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:text_value)
+  end
 end
